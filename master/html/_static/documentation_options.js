0ce969ce15e8cb5b86343b32eaacfda119aee0b9
abdes <abdes@users.noreply.github.com>
2021-11-06 18:41:51 +0000

    deploy: b52da30296df9002effc4ffe544cee971610170e

--- master/html/_static/documentation_options.js
+++ master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0 (261080c4acb5)',
+    VERSION: '2.0.1 (b52da30296df)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
