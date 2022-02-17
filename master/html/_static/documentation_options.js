3318cf259ca4200ddbfe08ae6024eb6d5cb96dae
abdes <abdes@users.noreply.github.com>
2022-02-17 19:46:06 +0000

    deploy: b50a2d2a460662e7d8baa9a491b1939fe7a5ef69

---
+++ master/html/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '4.0.2 (b50a2d2a4606)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true
+};
