571ac3f7b24b1805ebad46c3ccf38b13aeee72ae
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-10-21 19:33:23 +0400

    fix: api export generated header does not respect project layout
    The api header location should mimic the structure of the project. If a module is
    located deep in the project tree, then the generated file should be located in a
    sub-tree exactly the same than the module itself.
    
    BREAKING

--- cmake/BuildHelpers.cmake
+++ cmake/BuildHelpers.cmake
@@ -8,15 +8,29 @@
 # Build Helpers to simplify target creation.
 # ------------------------------------------------------------------------------
 
-function(asap_generate_export_headers target include_dir)
-  # Set API export file and macro
-  string(MAKE_C_IDENTIFIER ${target} TEMPLATE_TARGET_ID)
-  string(TOUPPER ${TEMPLATE_TARGET_ID} TEMPLATE_TARGET_ID)
-  set(export_file "include/${include_dir}/${target}_export.h")
-  set(template_export_file "include/${include_dir}/${target}_api.h")
-  set(TEMPLATE_TARGET "${target}")
-  set(TEMPLATE_INCLUDE_DIR "${include_dir}")
+function(identifier_to_upper_snake_case identifier)
+  set(result)
+  string(MAKE_C_IDENTIFIER "${identifier}" c_identifier)
+  string(REGEX MATCHALL "^[A-Z][A-Z0-9]*[a-z0-9]*" words ${c_identifier})
+  foreach(word IN LISTS words)
+    if(word STREQUAL "_")
+      string(APPEND result "_")
+    else()
+      string(TOUPPER "${word}" word_upper)
+      list(APPEND result "${word_upper}")
+    endif()
+  endforeach()
+  string(JOIN "_" out ${result})
+  set(base_name "${out}" PARENT_SCOPE)
+  string(JOIN "/" out ${result})
+  string(TOLOWER ${out} out)
+  set(template_include_dir "${META_PROJECT_ID_LOWER}/${out}" PARENT_SCOPE)
+endfunction()
 
+function(asap_generate_export_headers target)
+  # Set API export file and macro
+  identifier_to_upper_snake_case(${target})
+  set(export_file "include/${template_include_dir}/api_export.h")
   # Create API export headers
-  generate_export_header(${target} EXPORT_FILE_NAME ${export_file} EXPORT_MACRO_NAME ${TEMPLATE_TARGET_ID}_API)
+  generate_export_header(${target} BASE_NAME ${base_name} EXPORT_FILE_NAME ${export_file} EXPORT_MACRO_NAME ${base_name}_API)
 endfunction()
